109bf374c45a89f4f1d1369c3e79692af43abc84
Nikhil <hk5561111212@gmail.com>
2025-01-17 02:14:39 +0530

    Added: Video banner slider

--- app/ui/Banner.tsx
+++ app/ui/Banner.tsx
@@ -1,24 +1,64 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
-// import bannerImage from '@/public/Hero Images/hero.webp'
-import Link from 'next/link'
+import Autoplay from "embla-carousel-autoplay"
+
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 const bannerImage = 'https://res.cloudinary.com/dgywanyap/image/upload/v1736448623/WEB_HALO_2024_December_Week_51_Hero_Banner_Desktop_01-copy_e97axa.jpg'
+const bannerVideo = "https://res.cloudinary.com/dgywanyap/video/upload/v1737058576/image-video_sigwfi.mp4"
+const bannerVideo2 = "https://res.cloudinary.com/dgywanyap/video/upload/v1737059864/6009596_4k_Video_Attractive_1920x1080_ofh1jc.mp4"
 
-type Props = {}
+const Banner = () => {
 
-const Banner = (props: Props) => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 10000, stopOnInteraction: true })
+  )
+
   return (
-    <div className='h-[800px] w-auto -mt-[93px] relative -z-0 overflow-hidden flex items-end'>
-        <div className='flex gap-5 sm:px-8 h-fit w-full justify-between items-center z-10 relative mb-4'>
-           <h2 className='text-[#fff] text-[40px] leading-[50px] uppercase flex-1'>The Sale.</h2>
-           <p className='text-white block text-lg flex-1 text-center'>SAVE UP TO 50% ON EVERYTHING</p>
-           <div className='flex-1 flex justify-end items-center'>
-           <Link className='bg-white px-5 py-3 text-sm uppercase text-black w-fit rounded-full' href='/pages/skincare'>Shop Now</Link>
-           </div>
-        </div>
-        <Image className='absolute top-0 left-0 h-full w-full object-cover' width={1920} height={1080} src={bannerImage} alt='banner Image' />
+    <div className='h-screen w-auto -mt-[93px] relative -z-0 overflow-hidden flex items-end'>
+      <Carousel 
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+plugins={[plugin.current]}
+className="w-full"
+onMouseEnter={plugin.current.stop}
+onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent className='h-full'>
+          <CarouselItem className='h-full'>
+          <Video prop={bannerVideo}/>
+          </CarouselItem>
+          <CarouselItem className='h-full'> 
+            {/* <Image className='object-cover h-full' src={bannerImage} width={1920} height={1080} alt='banner Image' /> */}
+            <Video prop={bannerVideo2}/>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
+  )
+}
+
+
+const Video = ({prop}) => {
+  return (
+    <video width="1920" height="1080" autoPlay loop muted playsInline preload="none">
+      <source src={prop} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   )
 }
 
