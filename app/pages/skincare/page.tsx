ec5d69024b8e851f43bf8ca04c358c6dc547f231
Nikhil <hk5561111212@gmail.com>
2025-02-01 23:33:28 +0530

    Updated: Fetching product from api not directly prisma

--- app/pages/skincare/page.tsx
+++ app/pages/skincare/page.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import CollecionGrid from '@/components/collection/CollecionGrid'
-import { prisma } from '@/lib';
 import { Products } from '@/app/types/cart';
 
-async function getAllProducts() {
+const Page = () => {
+    const [products, setProducts] = useState<Products[]>([]);
 
-    const res = await prisma.product.findMany();
-    console.log(res)
-    return res;
-}
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch("/api/admin/getproducts");
+                if (res.ok) {
+                    const data = await res.json();
+                    setProducts(data);
+                } else {
+                    console.error("Error fetching products");
+                }
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
 
-const Page = async () => {
-    const products: Products[] = await getAllProducts();
+        fetchProducts();
+    }, []);
+
     return (
         <div className='max-w-screen-2xl px-12 m-auto'>
             <CollecionGrid products={products} />
